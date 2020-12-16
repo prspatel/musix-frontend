7f1952d36d97da96061062816cee43b778014ff5
Preet <pspatel1312@gmail.com>
2020-12-16 17:34:27 -0500

    pagination added to viewall

--- src/components/pages/viewAll.js
+++ src/components/pages/viewAll.js
@@ -17,13 +17,22 @@ export default function ViewAll() {
     const isLikedCollex = window.location.pathname.startsWith("/likedCollex");
     const isCollex = window.location.pathname.startsWith("/allCollex");
     const [playlistOrCollex, setPorC] = useState("");
+
+
+    //implementing pagination
+    //const [posts, setPosts] = useState([]); this is my data
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10);
+
+
     useEffect(() => {
         const fetchData = async () => {
             const userId = userData.user.id;
 
             if (isPlaylists) {
                 const result = await axios.get(
-                    `/api/playlist/playlists/${userId}`,
+                    /playlist/playlists / ${ userId },
                 );
                 setData(result.data);
                 setPorC("playlist")
@@ -31,7 +40,7 @@ export default function ViewAll() {
             }
             else if (isLikedPlaylists) {
                 const result = await axios.get(
-                    `/api/playlist/likedPlaylists/${userId}`,
+                    /playlist/likedPlaylists / ${ userId },
                 );
                 setData(result.data);
                 setPorC("playlist");
@@ -39,46 +48,60 @@ export default function ViewAll() {
             }
             else if (isLikedCollex) {
                 const result = await axios.get(
-                    `/api/collex/likedCollex/${userId}`,
+                    /collex/likedCollex / ${ userId },
                 );
                 setPorC("collex")
                 setData(result.data);
-                
+
             }
             else if (ismyCollex) {
                 const result = await axios.get(
-                    `/api/collex/myCollex/${userId}`,
+                    /collex/myCollex / ${ userId },
                 );
                 setPorC("collex")
                 setData(result.data);
             }
             else {
                 const result = await axios(
-                    '/api/collex/all',
+                    '/collex/all',
                 );
                 setData(result.data.collexs);
                 setPorC("collex");
 
             }
+            setLoading(false);
+
         };
 
         fetchData();
     }, []);
+
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
     function displayData() {
-        let cards = <h2 style={{ textAlign: "center", fontStyle: "italic", fontFamily: "roboto, sans-serif", marginTop: "12px" }}> You do not have any {playlistOrCollex} </h2>;
-        console.log(data);
-        if (data.length!=0) {
-            cards = data.map((item, id) => {
+        if (loading) {
+            return <h2>Loading...</h2>;
+        }
+        let cards = <h2 style={{ textAlign: "center", fontStyle: "italic", fontFamily: "roboto, sans-serif", marginTop: "12px" }}> You do not have any data</h2>;
+        if (currentPosts.length != 0) {
+            cards = currentPosts.map((item, id) => {
                 return (
                     <Card style={{ width: '14rem', margin: "2%", borderRadius: "24px" }} key={item._id} >
-                        <Card.Img variant="top" src={item.cover} height="200" width="150" style={{ borderRadius: "24px"}} />
+                        <Card.Img variant="top" src={item.cover} height="200" width="150" style={{ borderRadius: "24px" }} />
                         <Card.Body>
                             <Card.Title><Link to={`/${playlistOrCollex}/${item._id}`}>{item.name}</Link></Card.Title>
                             <Card.Text>
                                 {item.likes} Likes
                             </Card.Text>
                         </Card.Body>
-                        
+
                     </Card>
                 )
             });
@@ -89,20 +112,47 @@ export default function ViewAll() {
     return (
         <>
             <div className="viewall-background">
-            <Nav />
-            <div className ="viewall-body">
-                <div className="viewall-header">
-                    <div >
-                        <h1 className="viewall-topic">{isLikedPlaylists ? "Your Liked Playlists" : (isPlaylists ? "Your Playlists" : (isLikedCollex ? "Your Liked Collex" : (ismyCollex ? "Your Created Collexs" : "Entire Collex Gallery")))}</h1>
+                <Nav />
+                <div className="viewall-body">
+                    <div className="viewall-header">
+                        <div >
+                            <h1 className="viewall-topic">{isLikedPlaylists ? "Your Liked Playlists" : (isPlaylists ? "Your Playlists" : (isLikedCollex ? "Your Liked Collex" : (ismyCollex ? "Your Created Collexs" : "Entire Collex Gallery")))}</h1>
                         </div>
-                    <hr className="viewAll-solid" />
+                        <hr className="viewAll-solid" />
+                    </div>
+                    <div className="display-cards">
+                        {displayData()}
+                    </div>
+                    <Pagination
+                        postsPerPage={postsPerPage}
+                        totalPosts={data.length}
+                        paginate={paginate}
+                    />
                 </div>
-                <div className="display-cards">
-                    {displayData()}
-                </div>
-            </div>
-            <Footer />
+                <Footer />
             </div>
         </>
+    );
+}
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav>
+            <ul className='pagination'>
+                {pageNumbers.map(number => (
+                    <li key={number} className='page-item'>
+                        <a onClick={() => paginate(number)} className='page-link'>
+                            {number}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 }
